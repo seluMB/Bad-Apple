bd54f6dede2502288b1230cca39ab468458d0fdb
lilithisa <101096537+lilithisa@users.noreply.github.com>
2022-06-11 17:32:24 +0200

    Bullet Mechanic
    Bullet mechanic added

---
+++ Bad Apple/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	float moveSpeed = 7f;
+
+	Rigidbody2D rb;
+
+	GameObject target;
+	Vector2 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		target = GameObject.FindGameObjectWithTag("Player");
+		moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+		rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
+		Destroy (gameObject, 3f);
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag.Equals ("Player")) {
+			Debug.Log ("Hit!");
+			Destroy (gameObject);
+		}
+	}
+
+}
--- Bad Apple/Assets/Scripts/EnemyShoot.cs
+++ Bad Apple/Assets/Scripts/EnemyShoot.cs
@@ -1,18 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyShoot : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class EnemyShoot : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	GameObject bullet;
+
+	float fireRate;
+	float nextFire;
+
+	// Use this for initialization
+	void Start () {
+		fireRate = 1f;
+		nextFire = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckIfTimeToFire ();
+	}
+
+	void CheckIfTimeToFire()
+	{
+		if (Time.time > nextFire) {
+			Instantiate (bullet, transform.position, Quaternion.identity);
+			nextFire = Time.time + fireRate;
+		}
+		
+	}
+
 }
