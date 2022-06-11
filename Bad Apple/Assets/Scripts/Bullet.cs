16ee4a83006b34c8f4df018b85b45b91cd2afee0
lilithisa <101096537+lilithisa@users.noreply.github.com>
2022-06-11 18:59:32 +0200

    LilithChanges

--- Bad Apple/Assets/Scripts/Bullet.cs
+++ Bad Apple/Assets/Scripts/Bullet.cs
@@ -2,26 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
 	public float moveSpeed = 7f;
 	Rigidbody2D rb;
 	GameObject target;
 	Vector2 moveDirection;
 
 	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
 		target = GameObject.FindGameObjectWithTag("Player");
 		moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
-		rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
-		Destroy (gameObject, 3f);
-	}
-	void OnTriggerEnter2D (Collider2D col)
-	{
-		if (col.gameObject.tag.Equals ("Player")) {
-			Debug.Log ("Hit!");
-			Destroy (gameObject);
-		}
+		rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+		Destroy(gameObject, 3f);
 	}
 
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			Debug.Log("Hit!");
+			Debug.Log("D");
+			Destroy(gameObject);
+		}
+
+	}
 }
