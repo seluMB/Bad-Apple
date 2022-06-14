c8ac6f78d52f9f82bd04c9fa06e88a4407851081
seluMB <selina.behrens@gmx.net>
2022-06-14 15:51:01 +0200

    UI hearts and collectibles

--- Bad Apple/Assets/Scripts/ItemCollector.cs
+++ Bad Apple/Assets/Scripts/ItemCollector.cs
@@ -5,16 +5,49 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int peaches = 0;
-    [SerializeField] private Text peachText;
+    public bool isImgOn;
+    public Image peach;
+    public Image lemon;
+    public Image strawberry;
+    public Image pear;
+
+    void Start()
+    {
+        peach.enabled = false;
+        lemon.enabled = false;
+        strawberry.enabled = false;
+        pear.enabled = false;
+        isImgOn = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Peach"))
         {
             Destroy(collision.gameObject);
-            peaches++;
-            peachText.text = ": " + peaches;
+            peach.enabled = true;
+            isImgOn = true;
+        }
+
+        if (collision.gameObject.CompareTag("Lemon"))
+        {
+            Destroy(collision.gameObject);
+            lemon.enabled = true;
+            isImgOn = true;
+        }
+
+        if (collision.gameObject.CompareTag("Strawberry"))
+        {
+            Destroy(collision.gameObject);
+            strawberry.enabled = true;
+            isImgOn = true;
+        }
+
+        if (collision.gameObject.CompareTag("Pear"))
+        {
+            Destroy(collision.gameObject);
+            pear.enabled = true;
+            isImgOn = true;
         }
     }
 }
